9b1d2413705ea7e533868e0b933fa03342aab4d7
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-26 19:42:24 +0200

    s

--- task/Program.cs
+++ task/Program.cs
@@ -39,9 +39,71 @@
                     { Id = 10, Title = "Lighter", Price = 150, Category = "Car" }
             };
 
-            //  
+            // Выбрать товары категории Mobile, цена которых превышает 1000 грн.
+            var result1 = goods1.Where(g => g.Category == "Mobile" && g.Price > 1000);
+            // Вывести в консоль.
+            Console.WriteLine("1) Выбрать товары категории Mobile, цена которых превышает 1000 грн.");
+            foreach (var good in result1)
+            {
+                Console.WriteLine(good);
+            }
+
+            // Вывести название и цену тех товаров, которые не относятся к категории Kitchen, цена которых превышает 1000 грн.
+            var result2 = goods1.Where(g => g.Category != "Kitchen" && g.Price > 1000);
+            // Вывести в консоль.
+            Console.WriteLine("2) Вывести название и цену тех товаров, которые не относятся к категории Kitchen, цена которых превышает 1000 грн.");
+            foreach (var good in result2)
+            {
+                Console.WriteLine(good);
+            }
+
+            // Вычислить среднее значение всех цен товаров.
+            var result3 = goods1.Average(g => g.Price);
+            // Вывести в консоль.
+            Console.WriteLine("3) Вычислить среднее значение всех цен товаров.");
+            Console.WriteLine(result3);
+
+            // Вывести список категорий без повторений.
+            var result4 = goods1.Select(g => g.Category).Distinct();
+            // Вывести в консоль.
+            Console.WriteLine("4) Вывести список категорий без повторений.");
+            foreach (var category in result4)
+            {
+                Console.WriteLine(category);
+            }
+
+            // Вывести названия и категории товаров в алфавитном порядке, упорядоченных по названию.
+            var result5 = goods1.OrderBy(g => g.Title).Select(g => new { g.Title, g.Category });
+            // Вывести в консоль.
+            Console.WriteLine("5) Вывести названия и категории товаров в алфавитном порядке, упорядоченных по названию.");
+            foreach (var good in result5)
+            {
+                Console.WriteLine(good);
+            }
+
+            // Посчитать суммарное количество товаров категорий Сar и Mobile.
+            var result6 = goods1.Where(g => g.Category == "Car" || g.Category == "Mobile").Count();
+            // Вывести в консоль.
+            Console.WriteLine("6) Посчитать суммарное количество товаров категорий Сar и Mobile.");
+            Console.WriteLine(result6);
+
+            // Вывести список категорий и количество товаров каждой категории.
+            var result7 = goods1.GroupBy(g => g.Category).Select(g => new { Category = g.Key, Count = g.Count() });
+            // Вывести в консоль.
+            Console.WriteLine("7) Вывести список категорий и количество товаров каждой категории.");
+            foreach (var category in result7)
+            {
+                Console.WriteLine(category);
+            }
+            
+
+
+
+
+
+
         }
-        
+
 
     }
 }
