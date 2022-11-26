167897c38452394455b685ecc60b330f67ac6942
blazhkevych.v <blazhkevych.v@gmail.com>
2022-11-26 19:36:22 +0200

    ы

--- task/Program.cs
+++ task/Program.cs
@@ -15,7 +15,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Good> goods1 = new List<Good>()
+            {
+                new Good()
+                    { Id = 1, Title = "Nokia 1100", Price = 450.99, Category = "Mobile" },
+                new Good()
+                    { Id = 2, Title = "Iphone 4", Price = 5000, Category = "Mobile" },
+                new Good()
+                    { Id = 3, Title = "Refregirator 5000", Price = 2555, Category = "Kitchen" },
+                new Good()
+                    { Id = 4, Title = "Mixer", Price = 150, Category = "Kitchen" },
+                new Good()
+                    { Id = 5, Title = "Magnitola", Price = 1499, Category = "Car" },
+                new Good()
+                    { Id = 6, Title = "Samsung Galaxy", Price = 3100, Category = "Mobile" },
+                new Good()
+                    { Id = 7, Title = "Auto Cleaner", Price = 2300, Category = "Car" },
+                new Good()
+                    { Id = 8, Title = "Owen", Price = 700, Category = "Kitchen" },
+                new Good()
+                    { Id = 9, Title = "Siemens Turbo", Price = 3199, Category = "Mobile" },
+                new Good()
+                    { Id = 10, Title = "Lighter", Price = 150, Category = "Car" }
+            };
+
+            //  
         }
+        
+
     }
 }
